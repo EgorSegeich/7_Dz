e2f8f117006b283c65cb8fb7a86c7ee7e8ba425e
Egor <aldes@list.ru>
2022-12-23 17:01:31 +0300

    3 task+

--- 3_task/Program.cs
+++ 3_task/Program.cs
@@ -15,14 +15,14 @@
 */
 
 
-void FillArray(int [,] arr)
+void FillArray(int[,] arr)
 {
     Random random = new Random();
-	for(int i=0; i<arr.GetLength(0); i++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for(int j=0; j<arr.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            arr[i,j] = random.Next(100,999);   
+            arr[i, j] = random.Next(100, 999);
         }
     }
 }
@@ -30,12 +30,12 @@ void FillArray(int [,] arr)
 
 void PrintArray(int[,] arr)
 {
-    
+
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            Console.Write(arr[i, j] +  " ");
+            Console.Write(arr[i, j] + " ");
         }
         Console.WriteLine();
     }
@@ -43,7 +43,7 @@ void PrintArray(int[,] arr)
 }
 
 int rowCount = 10;
-int  columnCount = 10;
+int columnCount = 10;
 int[,] array = new int[rowCount, columnCount];
 
 
@@ -53,7 +53,6 @@ Console.WriteLine();
 
 int[] array2 = new int[10];
 
-//for(int k = 0; k<10; k++)
 
 for (int i = 0; i < array.GetLength(1); i++)
 {
@@ -61,13 +60,23 @@ for (int i = 0; i < array.GetLength(1); i++)
     int a = 0;
     for (int j = 0; j < array.GetLength(0); j++)
     {
-        if(array[j,i]%10 == array[j,i]/100 )
+
+        if (array[j, i] % 10 == array[j, i] / 100)
         {
-        sum = sum + array[j,i];
-        Console.WriteLine(sum);
-        Console.WriteLine("a= "+ a);
-        a++;
+            sum = sum + array[j, i];
+            a++;
         }
-         
+
+    }
+    if (sum > 0)
+    {
+        array2[i] = sum / a;
+        Console.Write(array2[i] + " ");
+    }
+    else
+    {
+        array2[i] = 0;
+        Console.Write(array2[i] + " ");
     }
 }
+
